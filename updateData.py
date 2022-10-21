a29b094038906cf7b4a5a02c6916cd74d69a7a67
daffaelengi <116154450+daffaelengi@users.noreply.github.com>
2022-10-21 15:44:17 +0700

    Update Data
    Untuk mengupdate data pada file .txt agar bisa mencoba input-input lain

---
+++ updateData.py
@@ -0,0 +1,28 @@
+vertices = 18
+driverIndex = 0
+driverName = ""
+driverRating = ""
+jumlah = ""
+
+for i in range(vertices) :
+    ojekCount = int(input(f"Masukkan jumlah ojek di node ke-{i}: "))
+    jumlah += str(ojekCount) + "\n"
+    if (ojekCount != 0) :
+        for j in range(ojekCount) :
+            driverName += input("Nama driver " + str(j + 1) + " di node " + str(i) + ": ") + "\n"
+            driverRating += input("Rating: ") + "\n"
+            driverIndex += 1
+    print("")
+
+# print(jumlah)
+# print(driverName)
+# print(driverRating)
+
+with open('jumlah ojek.txt', 'w') as a :
+    a.write(jumlah)
+
+with open('namaOjek.txt', 'w') as a :
+    a.write(driverName)
+
+with open('ratingOjek.txt', 'w') as a :
+    a.write(driverRating)
